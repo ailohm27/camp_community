7086e29064c6b85d470461d53c4a6021500b4317
Alexis Lohman <alexis.lohman@ahead.com>
2025-02-27 17:22:06 -0600

    adding in page 2

--- main.py
+++ main.py
@@ -1,28 +1,23 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Hello",
+    page_title="Start Page",
     page_icon="👋",
 )
-
-st.write("# Welcome to Streamlit! 👋")
+st.write("# IRE Design Automation POC")
 
 st.sidebar.success("Select a demo above.")
 
 st.markdown(
     """
-    Streamlit is an open-source app framework built specifically for
-    Machine Learning and Data Science projects.
-    **👈 Select a demo from the sidebar** to see some examples
-    of what Streamlit can do!
-    ### Want to learn more?
-    - Check out [streamlit.io](https://streamlit.io)
-    - Jump into our [documentation](https://docs.streamlit.io)
-    - Ask a question in our [community
-        forums](https://discuss.streamlit.io)
-    ### See more complex demos
-    - Use a neural net to [analyze the Udacity Self-driving Car Image
-        Dataset](https://github.com/streamlit/demo-self-driving)
-    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+    This is the work of Alexis Lohman, Kaz Kazarnoff and Shanay Martin. 
+
+    Shanay presented the idea to the team to automate an IRE generation. For workstreams, she was focused on the creation of the RACI to be 
+    templatized to support the automatic scirpting to populate it. 
+    Kaz and Alexis support efforts in creating the python scripts, generating fake data and setting up the wireframe
+    for Streamlit.
+
+    Please flip through portions of the POC to understand the process flow and architecrtual diagram, in addition to the individual
+    components making up the rought draft of the populated IRE.
 """
 )
